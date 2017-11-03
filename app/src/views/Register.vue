058525a31f31339ee3c5fd2c217040af5228499a
smilebuz <smile_buz@163.com>
2017-11-03 15:06:02 +0800

    add

--- app/src/views/Register.vue
+++ app/src/views/Register.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="register">
     <div class="formContainer">
-      <el-form ref="form" :model="form" label-width="90px">
-        <el-form-item label="账号:" prop="account" class="form__item">
-          <el-input v-model="form.account" placeholder="6-20位英文、数字或者常用符号" size="small"></el-input>
+      <el-form ref="registerForm" :rules="rules" :model="registerForm" label-width="90px">
+        <el-form-item label="账号:" prop="username" class="form__item">
+          <el-input placeholder="6-20位英文、数字或者常用符号" size="small"
+            v-model="registerForm.username">
+          </el-input>
         </el-form-item>
         <el-form-item label="工作岗位:" class="form__item">
-          <el-select v-model="form.post" placeholder="请选择" size="small">
+          <el-select class="form__item-select" placeholder="请选择" size="small"
+            v-model="registerForm.job_title">
             <el-option
-              v-for="(option, index) in form.post_options"
+              v-for="(option, index) in registerForm.post_options"
               :key="index"
               :value="option">{{ option }}
             </el-option>
@@ -16,51 +19,69 @@
         </el-form-item>
         <el-form-item label="密码:" prop="password" class="form__item">
           <el-input placeholder="6-20位英文、数字或者常用符号" type="password"
-            v-model="form.password"
+            v-model="registerForm.password"
             size="small">
           </el-input>
         </el-form-item>
         <el-form-item label="工作单位:" class="form__item">
-          <el-input v-model="form.organization" size="small"></el-input>
+          <el-input v-model="registerForm.work_unit" size="small"></el-input>
         </el-form-item>
         <el-form-item label="确认密码:" prop="password_confirm" class="form__item">
           <el-input placeholder="请再次输入密码" type="password"
-            v-model="form.password_confirm"
+            v-model="registerForm.password_confirm"
             size="small">
           </el-input>
         </el-form-item>
         <el-form-item label="所属行业:" class="form__item">
-          <el-select v-model="form.industry" placeholder="请选择" size="small">
-            <el-option v-for="(option, index) in form.industries" :key="index" :value="option">{{ option }}</el-option>
+          <el-select class="form__item-select" placeholder="请选择" size="small"
+            v-model="registerForm.industry">
+            <el-option
+              v-for="(option, index) in registerForm.industries"
+              :key="index"
+              :value="option">{{ option }}
+            </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="姓名:" prop="name" class="form__item">
-          <el-input v-model="form.name" size="small"></el-input>
+        <el-form-item label="姓名:" prop="fullname" class="form__item">
+          <el-input v-model="registerForm.fullname" size="small"></el-input>
         </el-form-item>
         <el-form-item label="获知渠道:" class="form__item">
-          <el-select v-model="form.source" placeholder="请选择" size="small">
-            <el-option v-for="(option, index) in form.sources" :key="index" :value="option">{{ option }}</el-option>
+          <el-select class="form__item-select" placeholder="请选择" size="small"
+            v-model="registerForm.information_source">
+            <el-option
+              v-for="(option, index) in registerForm.sources"
+              :key="index"
+              :value="option">{{ option }}
+            </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="座机:" class="form__item">
-          <el-input v-model="form.telephone" size="small"></el-input>
-        </el-form-item>
+        <!--el-form-item label="座机:" class="form__item">
+          <el-input v-model="registerForm.telephone" size="small"></el-input>
+        </el-form-item-->
         <el-form-item label="所属地区:" class="form__item">
-          <el-input v-model="form.district" size="small"></el-input>
+          <el-input v-model="registerForm.area" size="small"></el-input>
         </el-form-item>
         <el-form-item label="手机:" class="form__item">
-          <el-input v-model="form.mobile" size="small"></el-input>
+          <el-input v-model="registerForm.cell_phone_number" size="small"></el-input>
         </el-form-item>
         <el-form-item label="采购计划:" class="form__item">
-          <el-select v-model="form.purchase" placeholder="请选择" size="small">
-            <el-option v-for="(option, index) in form.purchase_time" :key="index" :value="option">{{ option }}</el-option>
+          <el-select class="form__item-select" placeholder="请选择" size="small"
+            v-model="registerForm.purchase_plan">
+            <el-option
+              v-for="(option, index) in registerForm.purchase_time"
+              :key="index"
+              :value="option">{{ option }}
+            </el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="E-mail:" prop="email" class="form__item">
-          <el-input v-model="form.email" size="small"></el-input>
+          <el-input v-model="registerForm.email" size="small"></el-input>
         </el-form-item>
         <el-form-item label="地址:" class="form__item">
-          <el-input v-model="form.address" size="small"></el-input>
+          <el-input v-model="registerForm.address" size="small"></el-input>
+        </el-form-item>
+        <el-form-item label="" class="form__item form__item-placeholder">
+          <el-input size="small"></el-input>
         </el-form-item>
       </el-form>
       <div class="buttons">      
@@ -75,43 +96,74 @@
 import sendRequest from '../Api'
 
 export default {
-  name: 'Register',
   data: () => {
+    var validatorPassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else if (value.length < 6 || value.length > 20) {
+        callback(new Error('请输入6-20位英文、数字或者常用符号'))
+      } else {
+        if (this.registerForm.password_confirm !== '') {
+          this.$refs.registerForm.validateField('password_confirm')
+        }
+        callback()
+      }
+    }
+    var validatePwConfirm = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.registerForm.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
       header: 'INNOPRO专利精准推荐系统',
-      form: {
-        account: '',
+      registerForm: {
+        username: '',
         password: '',
-        password_confirm: '',
-        name: '',
-        telephone: '',
-        mobile: '',
+        fullname: '',
+        cell_phone_number: '',
         email: '',
         address: '',
-        post: '',
-        post_options: ['总经理', '总监', '助理'],
-        organization: '',
+        job_title: '',
+        work_unit: '',
         industry: '',
+        information_source: '',
+        area: '',
+        password_confirm: '',
+        post_options: ['总经理', '总监', '助理'],
         industries: ['建筑建材', '水利水电', '机械机电', '咨询服务'],
-        source: '',
         sources: ['互联网', '邮件', '微信', '熟人介绍'],
-        district: '',
-        purchase: '',
-        purchase_time: ['一个月内', '三个月内', '六个月內', '无']
+        purchase_plan: ['一个月内', '三个月内', '六个月內', '无']
+      },
+      registerParams: {
+        username: '',
+        password: '',
+        fullname: '',
+        cell_phone_number: '',
+        email: '',
+        address: '',
+        job_title: '',
+        work_unit: '',
+        industry: '',
+        information_source: '',
+        area: '',
+        purchase_plan: ''
       },
       rules: {
-        account: [
+        username: [
           { required: true, message: '请输入账号名称', trigger: 'blur' },
           { min: 6, max: 20, message: '长度在6到20个字符', trigger: 'blur' }
         ],
         password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 20, message: '长度在6到20个字符', trigger: 'blur' }
+          {validator: validatorPassword, trigger: 'blur'}
         ],
         password_confirm: [
-          { required: true, message: '请再次输入密码', trigger: 'blur' }
+          {validator: validatePwConfirm, trigger: 'blur'}
         ],
-        name: [
+        fullname: [
           { required: true, message: '请输入姓名', trigger: 'blur' }
         ],
         email: [
@@ -121,24 +173,24 @@ export default {
     }
   },
   methods: {
-    register: function () {
-      let params = {
-        username: this.form.account,
-        password: this.form.password,
-        fullname: this.form.name,
-        landline_phone_number: this.form.telephone,
-        cell_phone_number: this.form.mobile,
-        email: this.form.email,
-        address: this.form.address,
-        job_title: this.form.post,
-        work_unit: this.form.organization,
-        industry: this.form.industry,
-        information_source: this.form.source,
-        area: this.form.district,
-        purchase_plan: this.form.purchase
-      }
-      sendRequest.register.post(params).then(res => {
-        this.$router.push('Login')
+    submitRegisterParams: function () {
+      this.$refs['registerForm'].validate((valid) => {
+        if (valid) {
+          for (let prop in this.registerParams) {
+            if (this.registerParams.hasOwnProperty(prop)) {
+              this.registerParams[prop] = this.form[prop]
+            }
+          }
+          sendRequest.register.post(this.registerParams).then(data => {
+            this.$router.push('Login')
+          })
+        } else {
+          this.$message({
+            message: '注册信息验证失败',
+            type: 'error'
+          })
+          return false
+        }
       })
     }
   }
@@ -154,7 +206,6 @@ export default {
     width: 60%;
     form {
       display: flex;
-      justify-content: center;
       flex-wrap: wrap;
       overflow: auto;
       padding: 5%;
@@ -169,9 +220,22 @@ export default {
     }
   }
   .form__item {
-    margin-bottom: 0;
+    margin: 0 auto;
+    margin-bottom: 20px;
     float: left;
     flex-basis: 40%;
+    &:nth-child(2n+1) {
+      margin-right: 0;
+    }
+    &:nth-child(2n) {
+      margin-left: 0;
+    }
+  }
+  .form__item-placeholder {
+    visibility: hidden;
+  }
+  .form__item-select {
+    width: 100%;
   }
   .buttons {
     margin-top: 10px;
